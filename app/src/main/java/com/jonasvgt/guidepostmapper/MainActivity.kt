0484744f0a65e7003c32dd5db440a59a4ace8572
JonasVgt <git@jonasvoigt.de>
2024-03-13 11:46:22 +0200

    add simple map view

--- app/src/main/java/com/jonasvgt/guidepostmapper/MainActivity.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.jonasvgt.guidepostmapper.ui.theme.GuidepostMapperTheme
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    OsmMapView()
                 }
             }
         }
@@ -30,17 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun OsmMapView(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            MapView(context).apply {
+                setTileSource(TileSourceFactory.USGS_TOPO)
+                setMultiTouchControls(true)
+
+            }
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun OsmMapViewPreview() {
     GuidepostMapperTheme {
-        Greeting("Android")
+        OsmMapView()
     }
 }
