ddb66368609931b966450e18fabfecbecc4fd592
JonasVgt <git@jonasvoigt.de>
2024-03-13 16:14:45 +0200

    add simple request for the location permission

--- app/src/main/java/com/jonasvgt/guidepostmapper/MainActivity.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.jonasvgt.guidepostmapper
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.jonasvgt.guidepostmapper.ui.theme.GuidepostMapperTheme
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
@@ -29,7 +35,26 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        val requestLocationPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Toast.makeText(this, "Location permission was granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Location permission was denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show()
+        }else{
+            requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+
+
     }
+
 }
 
 @Composable
