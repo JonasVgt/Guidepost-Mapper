d406f585f1a9c605f81d35b06eea0518ff6c55a9
JonasVgt <git@jonasvoigt.de>
2024-03-14 14:52:21 +0200

    moved creation of mapView outside of composable

--- app/src/main/java/com/jonasvgt/guidepostmapper/MainActivity.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/MainActivity.kt
@@ -15,58 +15,63 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import com.jonasvgt.guidepostmapper.osmmap.OsmMapView
 import com.jonasvgt.guidepostmapper.ui.theme.GuidepostMapperTheme
-import org.osmdroid.api.IGeoPoint
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        org.osmdroid.config.Configuration.getInstance().userAgentValue  = applicationContext.packageName
+        org.osmdroid.config.Configuration.getInstance().userAgentValue =
+            applicationContext.packageName
         val locationManager = GpsMyLocationProvider(this)
+        val mapView = MapView(this).apply {
+            setTileSource(TileSourceFactory.MAPNIK)
+            setMultiTouchControls(true)
+            overlays.add(MyLocationNewOverlay(locationManager, this).apply { enableMyLocation() })
+            controller.setZoom(9.5)
+            controller.setCenter(GeoPoint(48.8583, 2.2944))
+        }
         setContent {
-            var mapCenter by remember { mutableStateOf<IGeoPoint>(GeoPoint(48.8583, 2.2944)) }
             GuidepostMapperTheme {
-                Scaffold (
-                    floatingActionButton = {
-                        FabToMyLocation(onClick = {
-                            val loc = locationManager.lastKnownLocation
-                            if(loc == null){
-                                Toast.makeText(this, "No Last Location", Toast.LENGTH_SHORT).show()
-                            }else{
-                                mapCenter = GeoPoint(loc)
-                            }
+                Scaffold(floatingActionButton = {
+                    FabToMyLocation(onClick = {
+                        val loc = locationManager.lastKnownLocation
+                        if (loc == null) {
+                            Toast.makeText(this, "No Last Location", Toast.LENGTH_SHORT).show()
+                        } else {
+                            mapView.controller.animateTo(GeoPoint(loc))
+                        }
 
-                        })
-                    }
-                )
-                {
-                    innerPadding -> OsmMapView(mapCenter, locationManager, onMapCenterChanged = {newMapCenter -> mapCenter = newMapCenter }, modifier = Modifier.padding(innerPadding))
+                    })
+                }) { innerPadding ->
+                    OsmMapView(mapView, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
 
-        val requestLocationPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()
-            ) { isGranted: Boolean ->
-                if (isGranted) {
-                    Toast.makeText(this, "Location permission was granted", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Location permission was denied", Toast.LENGTH_SHORT).show()
-                }
+        val requestLocationPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Location permission was granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Location permission was denied", Toast.LENGTH_SHORT).show()
             }
+        }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show()
-        }else{
+        } else {
             requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
 
@@ -77,7 +82,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun FabToMyLocation(onClick : () -> Unit ){
+fun FabToMyLocation(onClick: () -> Unit) {
     FloatingActionButton(
         onClick = onClick,
         shape = CircleShape,
--- app/src/main/java/com/jonasvgt/guidepostmapper/osmmap/OsmMapView.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/osmmap/OsmMapView.kt
@@ -3,29 +3,10 @@ package com.jonasvgt.guidepostmapper.osmmap
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import org.osmdroid.api.IGeoPoint
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 @Composable
-fun OsmMapView(mapCenter : IGeoPoint, locationManager: GpsMyLocationProvider, onMapCenterChanged: (IGeoPoint) -> Unit, modifier: Modifier = Modifier) {
+fun OsmMapView(mapView: MapView, modifier: Modifier = Modifier) {
 
-    AndroidView(
-        modifier = modifier,
-        factory = { context ->
-            MapView(context).apply {
-                setTileSource(TileSourceFactory.MAPNIK)
-                setMultiTouchControls(true)
-                overlays.add(MyLocationNewOverlay(locationManager, this).apply { enableMyLocation() })
-                controller.setZoom(9.5)
-                controller.setCenter(mapCenter)
-            }
-        },
-        update = {
-            it.controller.setCenter(mapCenter)
-            onMapCenterChanged(it.mapCenter)
-        }
-    )
+    AndroidView(modifier = modifier, factory = { _ -> mapView }, update = {})
 }
