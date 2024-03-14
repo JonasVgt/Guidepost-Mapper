968be3445b5aec00ef5ceaa7986e28cc9bd50223
JonasVgt <git@jonasvoigt.de>
2024-03-14 17:48:20 +0200

    add "hiking" map style

--- app/src/main/java/com/jonasvgt/guidepostmapper/osmmap/MapStyle.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/osmmap/MapStyle.kt
@@ -3,16 +3,12 @@ package com.jonasvgt.guidepostmapper.osmmap
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
-class MapStyle {
-
-    val tileSource: OnlineTileSourceBase = TileSourceFactory.MAPNIK
-        get() = field
-
-    val name: String = "default"
+class MapStyle(val name: String, val tileSource: OnlineTileSourceBase = TileSourceFactory.MAPNIK) {
 
     companion object {
-        val DEFAULT: MapStyle = MapStyle()
-        val ALL = arrayOf(this.DEFAULT)
+        val DEFAULT: MapStyle = MapStyle(name="Default", TileSourceFactory.MAPNIK)
+        val HIKING: MapStyle = MapStyle(name="Hiking",tileSource = TileSourceFactory.HIKEBIKEMAP)
+        val ALL = arrayOf(this.DEFAULT, this.HIKING)
     }
 
 }
