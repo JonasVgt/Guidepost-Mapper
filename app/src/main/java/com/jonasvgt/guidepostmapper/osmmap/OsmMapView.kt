8cea821689895855a245b38f077d1401363edda8
JonasVgt <git@jonasvoigt.de>
2024-03-15 01:13:36 +0200

    add correct lifecycle handling for mapview

--- app/src/main/java/com/jonasvgt/guidepostmapper/osmmap/OsmMapView.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/osmmap/OsmMapView.kt
@@ -1,8 +1,12 @@
 package com.jonasvgt.guidepostmapper.osmmap
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import org.osmdroid.views.MapView
 
 @Composable
@@ -11,6 +15,20 @@ fun OsmMapView(
     modifier: Modifier = Modifier,
     style: MapStyle = MapStyle.DEFAULT
 ) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { _ , event ->
+            when(event){
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                else -> {}
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
     AndroidView(
         modifier = modifier,
         factory = { _ -> mapView.apply { setTileSource(style.tileSource) } },
