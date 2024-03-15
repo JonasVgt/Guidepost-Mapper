089c9c127c686262dc99e3ee060fd806206051ef
JonasVgt <git@jonasvoigt.de>
2024-03-15 22:09:58 +0200

    add GuidepostHandler to select guideposts from osm data

---
+++ app/src/main/java/com/jonasvgt/guidepostmapper/data/osmapi/GuidepostHandler.kt
@@ -0,0 +1,24 @@
+package com.jonasvgt.guidepostmapper.data.osmapi
+
+import de.westnordost.osmapi.map.data.BoundingBox
+import de.westnordost.osmapi.map.data.Node
+import de.westnordost.osmapi.map.data.Relation
+import de.westnordost.osmapi.map.data.Way
+import de.westnordost.osmapi.map.handler.MapDataHandler
+
+class GuidepostHandler(private val handleGuidePost: (Node?) -> Unit) : MapDataHandler {
+
+    override fun handle(bounds: BoundingBox?) {}
+
+    override fun handle(node: Node?) {
+        if (node?.tags?.get("information") == "guidepost") {
+            handleGuidePost(node)
+        }
+    }
+
+    override fun handle(way: Way?) {}
+
+    override fun handle(relation: Relation?) {}
+
+
+}
