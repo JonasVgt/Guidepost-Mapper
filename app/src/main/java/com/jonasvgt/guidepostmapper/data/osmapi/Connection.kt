6e821c670464bf16acf161db3baaaadc6fdc0307
JonasVgt <git@jonasvoigt.de>
2024-03-15 11:52:48 +0200

    add method to fetch data from osm api

--- app/src/main/java/com/jonasvgt/guidepostmapper/data/osmapi/Connection.kt
+++ app/src/main/java/com/jonasvgt/guidepostmapper/data/osmapi/Connection.kt
@@ -1,12 +1,17 @@
 package com.jonasvgt.guidepostmapper.data.osmapi
 
 import de.westnordost.osmapi.OsmConnection
+import de.westnordost.osmapi.map.MapDataApi
+import de.westnordost.osmapi.map.data.BoundingBox
+import de.westnordost.osmapi.map.handler.MapDataHandler
 
 
 class Connection {
 
 
     private var connection : OsmConnection? = null
+    private var mapApi : MapDataApi? = null
+
 
     fun connect() {
         if (connection != null)
@@ -15,6 +20,16 @@ class Connection {
         connection = OsmConnection(
             API_URL, "GuidepostMapper", null
         )
+    }
+
+    fun fetchMapData(boundingBox: BoundingBox, dataHandler: MapDataHandler){
+        if(connection == null)
+            return
+
+        if(mapApi == null)
+            mapApi = MapDataApi(connection)
+
+        mapApi?.getMap(boundingBox, dataHandler)
     }
 
     companion object {
