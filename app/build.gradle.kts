72a3c7894f6380db3cf74dc085c2e0df50abc830
JonasVgt <git@jonasvoigt.de>
2024-03-19 13:42:46 +0200

    add DataStore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.osmdroid.android)
     implementation(libs.osmapi.map)
+    implementation(libs.androidx.datastore.preferences)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
